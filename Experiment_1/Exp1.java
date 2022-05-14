f5a0cffcfe8f3efae1281ca1f99c911ad23ccfac
Hatim Sawai <hatimsawai76@gmail.com>
2022-05-14 10:14:56 +0530

    updated files
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Experiment_1/Exp1.java
+++ Experiment_1/Exp1.java
@@ -1,5 +1,5 @@
 public class Exp1 {
     public static void main(String[] args) {
-        System.out.println("This is experiment-1 program-1!");
+        System.out.println("This is experiment-1 program-1");
     }
 }
