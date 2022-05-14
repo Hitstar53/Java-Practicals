0e3cee5b2f5fd1eacef6f97b7bc8750670874202
Hatim Sawai <hatimsawai76@gmail.com>
2022-05-14 12:38:51 +0530

    created exp1 related files
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Experiment_1/Prog1.java
+++ Experiment_1/Prog1.java
@@ -1,4 +1,4 @@
-public class Exp1 {
+public class Prog1 {
     public static void main(String[] args) {
         System.out.println("This is experiment-1 program-1");
     }
---
+++ Experiment_1/Prog3.java
@@ -0,0 +1,5 @@
+public class Prog3 {
+    public static void main(String[] args) {
+        
+    }
+}
