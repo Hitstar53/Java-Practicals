4b8138538d186a6d3053940c77708be8e25fe24a
Hatim Sawai <hatimsawai76@gmail.com>
2022-05-18 20:05:20 +0530

    updated files
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ Experiment_1/Prog3.java
@@ -0,0 +1,6 @@
+import java.util. *;
+import java.lang.Math;
+public class Prog3 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+}
---
+++ Experiment_2/Prog1.java
@@ -0,0 +1,7 @@
+import java.util. *;
+import java.lang.Math;
+public class Name {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+    }
+}
