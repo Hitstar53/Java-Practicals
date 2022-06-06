131f339685381149f60f9f770db45a4bf4596bad
Hatim Sawai <hatimsawai76@gmail.com>
2022-06-06 23:35:40 +0530

    updated files
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Experiment_3/Line.java
+++ Experiment_3/Line.java
@@ -6,7 +6,7 @@ class Line {
     Line(double m,double c) {
         this.m = m;
         this.c = c;
-        System.out.printf("Line Eq: y = %.0f x + %.0f",m,c);
+        System.out.printf("Line Eq: y = %.0fx + %.0f",m,c);
         System.out.printf("\ny = %.0f at x = %.0f",m*x + c,x);
     }
     //constructor 2
@@ -14,15 +14,15 @@ class Line {
         this.m = m;
         this.x1 = x1;
         this.y1 = y1;
-        System.out.printf("Line Eq: y - %.0f = %.0f (x - %.0f )",y1,m,x1);
-        System.out.printf("\ny = %.0f at x = %.0f",(m*(x-x1))+y1);
+        System.out.printf("Line Eq: y - %.0f = %.0f(x - %.0f)",y1,m,x1);
+        System.out.printf("\ny = %.0f at x = %.0f",(m*(x-x1))+y1,x);
     }
     Line(double x1,double y1,double x2,double y2) {
         this.x1 = x1;
         this.y1 = y1;
         this.x2 = x2;
         this.y2 = y2;
-        System.out.printf("Line Eq: (y - %.0f )/%.0f = (x - %.0f)/%.0f",y1,y1-y2,x2,x1-x2);
+        System.out.printf("Line Eq: (y - %.0f)/%.0f = (x - %.0f)/%.0f",y1,y1-y2,x2,x1-x2);
         System.out.printf("\ny = %.0f at x = %.0f",((x-x1)/(x1-x2))*(y1-y2)+y1,x);
     }
     public static void main(String[] args) {
