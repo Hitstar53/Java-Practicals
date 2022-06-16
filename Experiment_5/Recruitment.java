d3e3eb10cb74a04e3771bac34f31ceacdda17826
Hatim Sawai <hatimsawai76@gmail.com>
2022-06-16 09:29:07 +0530

    added Recruitment.java
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

---
+++ Experiment_5/Recruitment.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Recruitment {
+    String name;
+    Scanner sc = new Scanner(System.in);
+    Recruitment(String name){
+        this.name = name;
+    }
+}
+class Testing extends Recruitment {
+    String ans1,ans2,ans3,ans4,app_ans;
+    Scanner sc = new Scanner(System.in);
+    Testing() {
+        super();
+        ans1 = "";
+        ans2 = "";
+        ans3 = "";
+        ans4 = "";
+    }
+    void takeTest() {
+
+    }
+    int generateResult() {
+
+    }
+}
