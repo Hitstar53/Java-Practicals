c13453e4fe054be0711486f4a7a26af128eef6c6
Hatim Sawai <hatimsawai76@gmail.com>
2022-06-23 16:05:43 +0530

    prog 2 & 3 of exp 6
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Experiment_6/ABC.java
+++ Experiment_6/ABC.java
@@ -1,9 +1,6 @@
 import java.util.*;
 class Employee {
     double salary,bonus;
-    Employee(double salary) {
-        this.salary = salary;
-    }
     void getSalary() {
         salary = 10000;
     }
@@ -13,10 +10,10 @@ class Employee {
 }
 class Intern extends Employee {
     void getSalary() {
-        salary = (3*salary)/4;
+        salary = salary*0.75;
     }
     void getBonus() {
-
+        bonus = 690;
     }
 }
 class Clerk extends Employee {
@@ -24,7 +21,7 @@ class Clerk extends Employee {
         salary = salary/2;
     }
     void getBonus() {
-
+        bonus = 720;
     }
 }
 class Manager extends Employee {
@@ -32,12 +29,24 @@ class Manager extends Employee {
         salary = 2*salary;
     }
     void getBonus() {
-
+        bonus = 1000;
     }
 }
 public class ABC {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        Intern in = new Intern();
+        in.getSalary();
+        in.getBonus();
+        System.out.printf("Salary of Intern: $%.2f",in.salary+in.bonus);
+        Clerk cr = new Clerk();
+        cr.getSalary();
+        cr.getBonus();
+        System.out.printf("\nSalary of Clerk: $%.2f",cr.salary+cr.bonus);
+        Manager mg = new Manager();
+        mg.getSalary();
+        mg.getBonus();
+        System.out.printf("\nSalary of Employee: $%.2f",mg.salary+mg.bonus);
         sc.close();
     }
 }
