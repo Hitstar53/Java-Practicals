b65ee3a5c4d4782c68f3518be85c8a0a744448bd
Hatim Sawai <hatimsawai76@gmail.com>
2022-06-24 08:57:34 +0530

    updated files
    Signed-off-by: Hatim Sawai <hatimsawai76@gmail.com>

--- Experiment_6/finance.java
+++ Experiment_6/finance.java
@@ -20,24 +20,12 @@ class SBI_bank extends Bank {
         super(time, p);
     }
     void get_rate_of_interest() {
-        if(time>=7 && time<=14) {
-            rate = 3;
-        }
-        else if(time>=15 && time<=30) {
-            rate = 3;
-        }
-        else if(time>=31 && time<=45) {
-            rate = 3;
-        }
-        else if(time>=46 && time<=90) {
-            rate = 4.05;
-        }
-        else if(time>=91 && time<=120) {
-            rate = 4.10;
-        }
-        else if(time<=121 && time>=180) {
-            rate = 4.10;
-        }
+        if(time>=7 && time<=14) {rate = 3;}
+        else if(time>=15 && time<=30) {rate = 3;}
+        else if(time>=31 && time<=45) {rate = 3;}
+        else if(time>=46 && time<=90) {rate = 4.05;}
+        else if(time>=91 && time<=120) {rate = 4.10;}
+        else if(time<=121 && time>=180) {rate = 4.10;}
     }
     void display() {
         amt = (1+rate/100);
@@ -51,24 +39,12 @@ class AXIS_Bank extends Bank {
         super(time, p);
     }
     void get_rate_of_interest() {
-        if(time>=7 && time<=14) {
-            rate = 3.15;
-        }
-        else if(time>=15 && time<=30) {
-            rate = 3.15;
-        }
-        else if(time>=31 && time<=45) {
-            rate = 3.45;
-        }
-        else if(time>=46 && time<=90) {
-            rate = 4.05;
-        }
-        else if(time>=91 && time<=120) {
-            rate = 4.70;
-        }
-        else if(time<=121 && time>=180) {
-            rate = 5;
-        }
+        if(time>=7 && time<=14) {rate = 3.15;}
+        else if(time>=15 && time<=30) {rate = 3.15;}
+        else if(time>=31 && time<=45) {rate = 3.45;}
+        else if(time>=46 && time<=90) {rate = 4.05;}
+        else if(time>=91 && time<=120) {rate = 4.70;}
+        else if(time<=121 && time>=180) {rate = 5;}
     }
     void display() {
         amt = (1+rate/100);
@@ -82,24 +58,12 @@ class ICICI_Bank extends Bank {
         super(time, p);
     }
     void get_rate_of_interest() {
-        if(time>=7 && time<=14) {
-            rate = 3.10;
-        }
-        else if(time>=15 && time<=30) {
-            rate = 3.20;
-        }
-        else if(time>=31 && time<=45) {
-            rate = 3.50;
-        }
-        else if(time>=46 && time<=90) {
-            rate = 4.50;
-        }
-        else if(time>=91 && time<=120) {
-            rate = 4.70;
-        }
-        else if(time<=121 && time>=180) {
-            rate = 4.90;
-        }
+        if(time>=7 && time<=14) {rate = 3.10;}
+        else if(time>=15 && time<=30) {rate = 3.20;}
+        else if(time>=31 && time<=45) {rate = 3.50;}
+        else if(time>=46 && time<=90) {rate = 4.50;}
+        else if(time>=91 && time<=120) {rate = 4.70;}
+        else if(time<=121 && time>=180) {rate = 4.90;}
     }
     void display() {
         amt = (1+rate/100);
@@ -116,9 +80,15 @@ public class finance {
         int choice,flag,p;
         double time;
         while(true) {
-            System.out.println("Enter time period and Amount:");
-            time = sc.nextDouble();
+            System.out.print("Enter time period(days): ");
+            while(true) {
+                time = sc.nextDouble();
+                if(time<=120) {break;}
+                else {System.out.print("Max Time limit is: 120 Days! Please Re-Enter time period: ");}
+            }
+            System.out.print("Enter the Amount: ");
             p = sc.nextInt();
+            cal.add(Calendar.DATE,(int)time);
             System.out.println("Select 1 Bank:\n1 -> SBI Bank\n2 -> ICICI Bank\n3 -> AXIS Bank");
             choice = sc.nextInt();
             switch(choice) {
@@ -126,23 +96,24 @@ public class finance {
                     SBI_bank b1 = new SBI_bank(time,p);
                     b1.get_rate_of_interest();
                     b1.display();
+                    System.out.println("Your Amount will mature on: "+date.format(cal.getTime()));
                     break;
                 case 2:
                     ICICI_Bank b2 = new ICICI_Bank(time,p);
                     b2.get_rate_of_interest();
                     b2.display();
+                    System.out.println("Your Amount will mature on: "+date.format(cal.getTime()));
                     break;
                 case 3:
                     AXIS_Bank b3 = new AXIS_Bank(time,p);
                     b3.get_rate_of_interest();
                     b3.display();
+                    System.out.println("Your Amount will mature on: "+date.format(cal.getTime()));
                     break;
                 default:
-                    System.out.println("Invalid case!");
+                    System.out.println("Invalid Bank!");
                     break;
             }
-            cal.add(Calendar.DATE,(int)time);
-            System.out.println("Your Amount will mature on: "+date.format(cal.getTime()));
             System.out.println("Do you want to continue?(yes=1/0=no)");
             flag = sc.nextInt();
             if(flag==0) {
